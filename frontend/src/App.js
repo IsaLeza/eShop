e84841ffbb526206c048dfa4bc55fe75fb3e9e1c
Isaias Lezama Aponte <isaiaslezama@gmail.com>
2023-03-31 11:31:12 -0600

    h

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eShop</a>
+        
+      </header>
+      <main>Lista de Productos</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,43 @@
+const data ={
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brande: 'Nike',
+            rating: '4.5',
+            numberReviews: 10,
+            description:"high quality shirt",
+
+        },
+        {
+            name: 'Nike Slim pant',
+            slug: 'nike-slim-pant',
+            category:'Pants',
+            image:'/images/p1.jpg',
+            price: 25,
+            countInStock: 15,
+            brande: 'Nike',
+            rating: '4.5',
+            numberReviews: 14,
+            description:"high quality product",
+
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 65,
+            countInStock: 5,
+            brande: 'Adidas',
+            rating: '4.5',
+            numberReviews: 10,
+            description:"high quality shirt",
+
+        },
+    ],
+};
